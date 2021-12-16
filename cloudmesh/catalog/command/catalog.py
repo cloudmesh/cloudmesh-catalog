ba85f0e7efa255a4a49d371eab77874f03d4a9f0
Gregor von Laszewski <laszewski@gmail.com>
2021-12-16 09:53:34 -0500

    eanup and improve catalog manuall

--- cloudmesh/catalog/command/catalog.py
+++ cloudmesh/catalog/command/catalog.py
@@ -1,6 +1,5 @@
 from cloudmesh.shell.command import command
 from cloudmesh.shell.command import PluginCommand
-from cloudmesh.catalog.api.manager import Manager
 from cloudmesh.common.console import Console
 from cloudmesh.common.util import path_expand
 from pprint import pprint
@@ -19,22 +18,54 @@ class CatalogCommand(PluginCommand):
                 catalog list
                 catalog init --directory=DIR
                 catalog query --name=NAME
-                catalog table --attributes=ATTRIBUTES #string with commas i.e. 'id,name'
+                catalog table --attributes=ATTRIBUTES
+                catalog start
+                catalog stop
+                catalog status
 
           This command does some useful things.
 
           Arguments:
-              FILE   a file name
+              DIR   the directory path containing the entries
+              NAME  the name of the entry
+              ATTRIBUTES  string with commas i.e. 'id,name'
 
           Options:
-              -f      specify the file
+              --f      specify the file
+
+          Description:
+
+            catalog list
+              lists the content of the catalog
+
+            catalog init --directory=DIR
+                initializes the catalag from a directory
+
+            catalog query --name=NAME
+                queries the catalog by a named entry. This is just a preliminary
+                function and needs to be improved. YAMLDB provides a much better query.
+
+            catalog table --attributes=ATTRIBUTES
+                prints the content of the catalog in table form
+
+            catalog start --port=PORT
+                starts the catalog service
+
+            catalog stop
+                stops the catalog service
+
+            catalog status
+                returns the status of the service
 
         """
 
 
         # arguments.FILE = arguments['--file'] or None
 
-        map_parameters(arguments, "directory", "name", "attributes")
+        map_parameters(arguments,
+                       "directory",
+                       "name",
+                       "attributes")
       
 
         VERBOSE(arguments)
